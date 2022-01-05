8f9e7951c68a8f2fd772f2d47fde44ebe7263ac3
Tim B <t.beuke15433@hs-ansbach.de>
2022-01-05 19:42:26 +0100

    converts a string into an array

--- index.spec.js
+++ index.spec.js
@@ -2,15 +2,15 @@ const {calculateISBNChecksum} = require('./src/methods.js')
 
 describe('methods.js', () => {
 
-    it('should accept a string of a ten-digit-number as Parameter and return a String', () => {
-        inputString = "0000000000"
+    it('should convert an input-string of a ten-digit-number and return it as an array', () => {
+        inputString = "0123456789"
         var result = calculateISBNChecksum(inputString)
-        expect(result).toEqual(expect.stringContaining(""))
+        expect(result).toStrictEqual(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
     })
 
-    it('should accept a string of a ten-digit-number as Parameter and return a String', () => {
-        inputString = "0000000000"
+    it('should convert an input-string of a ten-digit-number and return it as an array', () => {
+        inputString = "9876543210"
         var result = calculateISBNChecksum(inputString)
-        expect(result).toEqual(expect.stringContaining(""))
+        expect(result).toStrictEqual(['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'])
     })
 })
--- src/methods.js
+++ src/methods.js
@@ -3,7 +3,7 @@ function checkISBN(checkSum) {
     console.log("checking...")
     checkSum = 5
     
-    let tenthSpot = inputFieldISBN.value.split("")
+    let tenthSpot = inputFieldISBN.value.split('')
     if(tenthSpot[9] === checkSum.toString()){
         userFeedback.textContent = "ISBN is valid"
         userFeedback.style.color = "green"
@@ -15,7 +15,7 @@ function checkISBN(checkSum) {
 }
 
 function calculateISBNChecksum (inputString){
-    return inputString
+    return inputString.split('')
 }
 module.exports = {
      checkISBN, calculateISBNChecksum
